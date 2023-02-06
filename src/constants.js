4903a7aa3b13ab1d79bb63e90443a4d604137939
gurudeep88 <gurudeep88@gmail.com>
2023-02-06 14:48:34 +0530

    cobrowing button added

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const POP_UP = {
+    LOGO : 'LOGO',
+    CLOSE : 'CLOSE',
+    GO: 'GO',
+    SEND: 'SEND',
+    COPY: 'COPY',
+    DONE: 'DONE'
+}
+export const SUCCESS_MESSAGE = 'Successfully Copied';
+
+export const FONT_FAMILY = {
+    DM_SANS: `'DM Sans', sans-serif`
+}
